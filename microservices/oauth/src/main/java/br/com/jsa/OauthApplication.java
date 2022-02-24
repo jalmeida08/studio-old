2c149a34ff3cab89de95f08e2b220705361052db
Jefferson Siqueira <jefferson08jsa@hotmail.com>
2022-02-24 09:57:01 -0300

    servidor de autenticacao

--- microservices/oauth/src/main/java/br/com/jsa/OauthApplication.java
+++ microservices/oauth/src/main/java/br/com/jsa/OauthApplication.java
@@ -2,10 +2,10 @@ package br.com.jsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-
-public class OauthApplication {
+public class OauthApplication  extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthApplication.class, args);
---
+++ microservices/oauth/src/main/java/br/com/jsa/infra/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package br.com.jsa.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@Order(1)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	http
+        	.antMatcher("/**")
+	        	.authorizeRequests()
+	        	.antMatchers("/oauth/authorize**", "/login**", "/error**")
+	        	.permitAll()
+        	.and()
+            	.authorizeRequests()
+            	.anyRequest().authenticated()
+        	.and()
+        		.formLogin().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+        	.inMemoryAuthentication()
+            .withUser("admin").password(passwordEncoder().encode("123456")).roles("USER");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
